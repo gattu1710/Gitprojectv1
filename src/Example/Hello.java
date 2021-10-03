2a7fb7ea5a9823c08e35c7b12912bd6f7e0c1ea1
gattussk <89243725+gattussk@users.noreply.github.com>
2021-10-03 16:52:41 +0530

    First commit

---
+++ src/Example/Hello.java
@@ -0,0 +1,14 @@
+package Example;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
